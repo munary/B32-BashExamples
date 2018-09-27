79eb0025c4b567d38bde1836937092f0738f8d89
Dan <asdsad@gmail.com>
2018-09-27 16:53:38 -0400

    fini la bombe

--- exercice3.sh
+++ exercice3.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+addSpace() {
+	for ((i=0;i<$1;i++))
+	do
+		echo -n " "
+	done
+}
 
-for ((
+for ((i=0;i<9;i++))
+do
+	addSpace $i
+	echo "@"
+	sleep 0.2
+	clear
+done
+
+addSpace 15
+echo "BOOM!"
