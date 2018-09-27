68258bbb4c49f3c27ec8e3aa00344a42013fda3a
Dan <asdsad@gmail.com>
2018-09-27 16:20:14 -0400

    pas fini le prog 2

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+menu() {
+	clear
+	echo "----------------"
+        echo "d = dictionnaire+"
+        echo "q = quitter"
+        echo "-----------------"
+	echo ""
+}
+
+lettre=s
+
+while [ $lettre != q ]; do
+	menu
+	echo ""
+	read -p "Entrez une lettre : " lettre
+	
+	if [[ $lettre
+done
+
+if 
+ls=`./exercice1.sh` 
+
